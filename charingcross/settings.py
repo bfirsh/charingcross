43cfd72065e4f2bbaf55e848b51cb83fd23c3f3b
Ben Firshman <ben@firshman.co.uk>
2016-04-07 16:53:18 -0700

    Make ALLOWED_HOSTS configurable
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- charingcross/settings.py
+++ charingcross/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import environ
-env = environ.Env(DEBUG=(bool, False),)
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, []),
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
